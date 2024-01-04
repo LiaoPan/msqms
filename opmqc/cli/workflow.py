17720d8983376364457e5e58be05589ef59e57d5
LiaoPan <LiaoPan_2015@163.com>
2024-01-04 11:06:22 +0800

    feat:add 3d positions

---
+++ opmqc/cli/workflow.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   Description :
+   Author :       LiaoPan
+-------------------------------------------------
+   Change Activity:
+                   2023/10/16:
+-------------------------------------------------
+"""
+__author__ = 'LiaoPan'
+
+def build_qc_pipeline(config_file):
+    """
+    Create the Nipype workflow.
+    Parameters
+    ----------
+    config_file
+
+    Returns
+    -------
+
+    """
+    pass
---
+++ opmqc/reports/report.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""<Explain your Codes>"""
