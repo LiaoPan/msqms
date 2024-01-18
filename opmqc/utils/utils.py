ff8a82a1c44a17e9c9a574a896f39b28a8ccb845
LiaoPan <LiaoPan_2015@163.com>
2024-01-19 00:33:47 +0800

    feat: format timedelta

--- opmqc/utils/utils.py
+++ opmqc/utils/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Utility functions"""
+
+import datetime
 import numpy as np
 
 def fill_zeros_with_nearest_value(arr):
@@ -11,3 +13,12 @@ def fill_zeros_with_nearest_value(arr):
         right_idx = non_zero_indices[non_zero_indices > idx][0] if np.any(non_zero_indices > idx) else np.inf
         arr[idx] = arr[left_idx] if (idx - left_idx) < (right_idx - idx) else arr[right_idx]
     return arr
+
+
+def format_timedelta(seconds):
+    """convert seconds to HH:MM:SS+MS"""
+    delta = datetime.timedelta(seconds=seconds)
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    formatted_time = "{:02}:{:02}:{:06.3f}".format(hours, minutes, seconds + delta.microseconds / 1e6)
+    return formatted_time
