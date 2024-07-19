43f4814e20a63bbe18baf62d9687c90bc80c215f
LiaoPan <liaopan_2015@163.com>
2024-07-19 14:37:14 +0800

    [docs]add comments

--- opmqc/constants.py
+++ opmqc/constants.py
@@ -4,13 +4,6 @@ from typing import TypeVar, Literal
 MEG_TYPE = TypeVar("MEG_TYPE", Literal['mag'], Literal['grad'])
 DATA_TYPE = TypeVar("DATA_TYPE", Literal['opm'], Literal['squid'])
 
-METRICS_COLUMNS = {
-    "time_domain": ['S', 'C', 'I', 'hjorth_mobility', 'hjorth_complexity', 'DFA'],
-    "frequency_domain": ['p3', 'p4', 'p5', 'p7', 'p8', 'p9', 'p11', 'p12'],
-    "fractal": ['PFD', 'KFD', 'HFD'],
-    "entropy": ['permutation_entropy', 'spectral_entropy', 'svd_entropy', 'Total_Entropy'],
-    "artifacts": ['BadChanRatio', 'BadSegmentsRatio', 'NaN_ratio', 'Flat_chan_ratio']}
-
 METRICS_CLASS_MAPPING = {"FreqDomainMetric": "frequency_domain",
                          "StatsDomainMetric": "stats_domain",
                          "EntropyDomainMetric": "entropy",
@@ -18,6 +11,15 @@ METRICS_CLASS_MAPPING = {"FreqDomainMetric": "frequency_domain",
                          "CustomMetric": "custom_domain"}
 
 # For HTML Report Display
+## Report Metrics
+METRICS_COLUMNS = {
+    "time_domain": ['S', 'C', 'I', 'hjorth_mobility', 'hjorth_complexity', 'DFA'],
+    "frequency_domain": ['p3', 'p4', 'p5', 'p7', 'p8', 'p9', 'p11', 'p12'],
+    "fractal": ['PFD', 'KFD', 'HFD'],
+    "entropy": ['permutation_entropy', 'spectral_entropy', 'svd_entropy', 'Total_Entropy'],
+    "artifacts": ['BadChanRatio', 'BadSegmentsRatio', 'NaN_ratio', 'Flat_chan_ratio']}
+
+
 ## For metric category mappings
 METRICS_REPORT_MAPPING = {"time_domain": "Time Metrics",
                           "frequency_domain": "Frequency Metrics",
